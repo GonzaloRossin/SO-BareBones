6db9f40b232d5d0351d4926df4e71fb78fb694a4
GonzaloRossin <grossin@itba.edu.ar>
2020-11-05 12:58:53 -0300

    auxiliary functions added

--- RowDaBoat-x64barebones/Userland/SampleCodeModule/Include/Lib.h
+++ RowDaBoat-x64barebones/Userland/SampleCodeModule/Include/Lib.h
@@ -7,11 +7,12 @@ void print(char *buffer);
 void putChar(char c);
 int strlen(char *str);
 int strcmp(const char *str1, const char *str2);
-void strncpy(char *source, char *dest, int size);
+void strncpy(char *source, char *dest, int index, int size);
 void newLine();
 void numToChar(double num, char * c);
 void putChar(char c);
 void actionCall(int action);
+uint8_t* strToNumHex(char * str);
 
 //Structure for drawing
 //#pragma pack(push)
