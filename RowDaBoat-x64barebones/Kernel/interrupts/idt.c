3729605210032fef23077d9c429de0706fb688fc
Ian Mejalelaty <imejalelaty@itba.edu.ar>
2020-11-08 13:16:19 -0300

    .

--- RowDaBoat-x64barebones/Kernel/interrupts/idt.c
+++ RowDaBoat-x64barebones/Kernel/interrupts/idt.c
@@ -59,9 +59,9 @@ void loadIDT()
 {
 	//disable interrupts
 	_cli();
-	setupEntry(0x20, (uint64_t) &irq0Handler);  //Interrupt del timertick
+	setupEntry(0x20, (uint64_t) &irq0Handler);  // Interrupt del timertick
 	setupEntry(0x21,(uint64_t) &irq1Handler); 	// Interrupt del teclado
-	setupEntry(0x80, (uint64_t)&int_80); 		//Int 80
+	setupEntry(0x80, (uint64_t)&int_80); 		// Int 80
 	setupEntry(0x00, (uint64_t)&exception0);  	//exception 0 division by 0
 	setupEntry(0x06, (uint64_t)&exception6);  	//exception 6 invalid opcode
 	// all bits in one disable that irq
--- RowDaBoat-x64barebones/Userland/SampleCodeModule/chess_draw.c
+++ RowDaBoat-x64barebones/Userland/SampleCodeModule/chess_draw.c
@@ -1,6 +1,3 @@
-// Created from bdf2c Version 3, (c) 2009, 2010 by Lutz Sammer
-//	License AGPLv3: GNU Affero General Public License version 3
-
 #ifndef CHESS_BITMAPS_H
 #define CHESS_BITMAPS_H
 
